3fbfc0ebaea4411b47edabae89155a2792c25fd4
Sven Mol <s.mol@student.utwente.nl>
2022-06-11 20:58:47 +0200

    Add helper types

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,18 @@
+export type Constructor<T, A extends any[]> = new (...args: A) => T;
+
+export type Computed<T extends {}> = {
+  [key in keyof T]: T[key] | (() => T[key]);
+};
+
 export type Either<A extends {}, B extends {}> =
   | (A & { [key in keyof B]?: never })
   | (B & { [key in keyof A]?: never });
+
+export function resolve<T>(computable: Computed<T>): T {
+  return Object.fromEntries(
+    Object.entries(computable).map(([key, value]) => [
+      key,
+      typeof value === "function" ? value() : value,
+    ])
+  ) as T;
+}
