bd032768609f8a1cfc8e3ad341412ef12733b481
Sven Mol <s.mol@student.utwente.nl>
2022-06-11 21:30:59 +0200

    Allow creation of CookieLike objects via constructor

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,4 @@
+import { resolve } from "./helpers";
 import Manager from "./manager";
 import { CookieLike, defaults, Options } from "./options";
 
@@ -9,5 +10,13 @@ const handler: ProxyHandler<{}> = {
   ownKeys: (_) => Manager.keys(),
   getOwnPropertyDescriptor: (_, property: string) =>
     Manager.has(property) ? Manager.propertyDescription : undefined,
+  construct: (
+    _,
+    [value, options]: [string, Partial<Options>]
+  ): string & Options => {
+    const resolved = { ...resolve(defaults), ...options } as Options;
+    if ("expires" in defaults) delete resolved.maxAge;
+    return Object.assign(value, resolved);
+  },
 };
 export default handler;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import handler from "./handler";
+import { Constructor } from "./helpers";
+import Manager from "./manager";
+import { CookieLike, Options } from "./options";
 
-type PublicType = { [key: string]: string };
+type PublicType = Constructor<CookieLike, [string, Partial<Options>]> & {
+  [key: string]: CookieLike;
+};
 
-const Cookie: PublicType = new Proxy({}, handler) as unknown as PublicType;
+const Cookie: PublicType = new Proxy(Manager, handler) as unknown as PublicType;
 
 export default Cookie;
