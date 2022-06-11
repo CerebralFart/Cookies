5c0d9638bb0e94c88e81a4331f2f6410a3e6c6bd
Sven Mol <s.mol@student.utwente.nl>
2022-06-11 22:20:08 +0200

    Add testing of new behaviours

---
+++ cypress/integration/advanced.spec.js
@@ -0,0 +1,39 @@
+const Cookie = require("../../dist");
+/// <reference types="cypress" />
+
+describe("Cookie Management", () => {
+  beforeEach(() => {
+    cy.visit("/cypress/test.html");
+    cy.clearCookies();
+  });
+
+  it("Can be used as string", async () => {
+    const value = "value";
+    const cookie = new Cookie(value);
+    for (let i = 0; i < value.length; i++) {
+      expect(cookie.charAt(i)).to.eq(value[i]);
+    }
+  });
+
+  it("Can set cookies", async () => {
+    Cookie.name = new Cookie("value");
+    cy.getCookie("name").should("have.property", "value", "value");
+  });
+
+  it("Can't set cookies in another folder", async () => {
+    Cookie.name = new Cookie("value", { path: "/wrong-folder/" });
+    cy.getCookie("name").should("be.null");
+  });
+
+  it("Respects expiry", async () => {
+    Cookie.name = new Cookie("value", { expires: new Date("01-01-1970") });
+    cy.getCookie("name").should("be.null");
+  });
+
+  it("Respects max age", async () => {
+    Cookie.name = new Cookie("value", { maxAge: 1 });
+    cy.getCookie("name").should("have.property", "value", "value");
+    await new Promise((res) => setTimeout(res, 2000));
+    cy.getCookie("name").should("be.null");
+  });
+});
